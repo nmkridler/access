789547006e07974959ec3642e3604a492b85e4f4
Nick <nmkridler@gmail.com>
2013-06-23 15:22:58 -0500

    latest version

--- fileio.py
+++ fileio.py
@@ -20,70 +20,46 @@ def buildTrips(x):
 				pairs.append([x[i],x[j],x[k]])	
 	return pairs
 
-def buildQuads(x):
-	pairs = []
-	for i in xrange(len(x)):
-		for j in xrange(i+1,len(x)): 
-			for k in xrange(j+1,len(x)):
-				for l in xrange(k+1,len(x)):
-					pairs.append([x[i],x[j],x[k],x[l]])	
-	return pairs
-
-def addFourgrams(df,pairs):
-	h = df.columns
-	keys = []
-	total = 0
-	for pair in pairs:
-		key = '%s.%s.%s.%s'%(pair[0],pair[1],pair[2],pair[3])
-		keys.append(key)
-		df[key] = map(lambda x: '%i,%i,%i,%i'%(x[0],x[1],x[2],x[3]),
-			zip(df[pair[0]],df[pair[1]],df[pair[2]],df[pair[3]]))
-		y = df[key].value_counts().order()[::-1]
-		#counts = int(np.sum(y >= threshold))
-		counts = int(y.size*0.38)
-		total += counts
-		index = np.arange(y.size,dtype='int32')
-		index[counts:] = counts
-		df = pd.merge(df,pd.DataFrame({key:y.index,key+'Ids':index}),how='inner',on=key,sort=False)
-	print total
-	return df.ix[:,[c for c in df.columns if c not in keys]]
-
 def addTrigrams(df,pairs):
 	h = df.columns
-	keys = []
+	df['ID'] = map(lambda x: "%s.%06i"%(x[0],x[1]),
+		zip(['train']*32769 + ['test']*58921, range(32769) + range(58921)))
+	keys = [ col for col in h]
 	total = 0
 	for pair in pairs:
 		key = '%s.%s.%s'%(pair[0],pair[1],pair[2])
+		keys.append(key)
 		df[key] = map(lambda x: '%i,%i,%i'%(x[0],x[1],x[2]),
 			zip(df[pair[0]],df[pair[1]],df[pair[2]]))
 		y = df[key].value_counts().order()[::-1]
-		#counts = int(np.sum(y >= threshold))
-		counts = int(y.size*0.4)
+		counts = int(y.size*0.42)
 		total += counts
 		index = np.arange(y.size,dtype='int32')
 		index[counts:] = counts
 		df = pd.merge(df,pd.DataFrame({key:y.index,key+'Ids':index}),how='inner',on=key,sort=False)
-		keys.append(key)
 	print total
 	return df.ix[:,[c for c in df.columns if c not in keys]]
 
 
 def addBigrams(df,pairs):
 	h = df.columns
-	keys = []
 	total = 0
+	df['ID'] = map(lambda x: "%s.%06i"%(x[0],x[1]),
+		zip(['train']*32769 + ['test']*58921, range(32769) + range(58921)))
+	keys = [ col for col in h]
 	for pair in pairs:
 		key = '%s.%s'%(pair[0],pair[1])
+		keys.append(key)
 		df[key] = map(lambda x: '%i,%i'%(x[0],x[1]),
 			zip(df[pair[0]],df[pair[1]]))
 		y = df[key].value_counts().order()[::-1]
-		#counts = int(np.sum(y >= threshold))
-		counts = int(y.size*0.5)
+		counts = int(np.sum(y >= 1000))
+		if counts == 0:
+			continue
 		total += counts
 		index = np.arange(y.size,dtype='int32')
 		index[counts:] = counts
 		df = pd.merge(df,pd.DataFrame({key:y.index,key+'Ids':index}),how='inner',on=key,sort=False)
-		keys.append(key)
 	print total
 	return df.ix[:,[c for c in df.columns if c not in keys]]
 
@@ -92,13 +68,14 @@ def threshold(df):
 	col = dict([(y+'Ids',y) for y in h])
 	df['ID'] = map(lambda x: "%s.%06i"%(x[0],x[1]),
 		zip(['train']*32769 + ['test']*58921, range(32769) + range(58921)))
-	return df
 	col['ID'] = 'ID'
+	return df
 	for c in h:
 		y = df[c].value_counts().order()[::-1]
-		counts = int(y.size*1.0)
+		counts = int(y.size*0.44)
+		#counts = int(np.sum(y >= 30))
 		index = np.arange(y.size,dtype='int32')
-		#index[counts:] = counts
+		index[counts:] = counts
 		df = pd.merge(df,pd.DataFrame({c:y.index,c+'Ids':index}),how='inner',on=c,sort=False)
 	df = df.ix[:,[c for c in df.columns if c not in h]]
 	df = df.rename(columns=col)
@@ -109,12 +86,14 @@ class Fileio(object):
 		df = pd.read_csv(filename)
 		pairs = buildPairs(df.columns)
 		trips = buildTrips(df.columns)
-		quads = buildQuads(df.columns)
 		self.header = df.columns
-		self.df = threshold(df)
+		self.df = threshold(df.copy())
+		#print self.df.shape
+		#self.df = pd.merge(self.df,addBigrams(df.copy(),pairs),how='inner',on='ID')
+		self.df = pd.merge(self.df,addTrigrams(df.copy(),trips),how='inner',on='ID')
+		#self.df = addTrigrams(df.copy(),trips)
+
 		#self.df = addBigrams(self.df,pairs)
-		self.df = addTrigrams(self.df,trips)
-		#self.df = addFourgrams(self.df,quads)
 		self.encoder = OneHotEncoder()
 		usecols = [c for c in self.df if c != 'ID']
 		self.encoder.fit(np.array(self.df.ix[:,usecols],dtype='float'))
--- predict.py
+++ predict.py
@@ -12,23 +12,34 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 from sklearn.linear_model import SGDClassifier
+from sklearn.ensemble import RandomForestClassifier
 def main():
 
 	f = fileio.Fileio('../data/alldata.csv')
 	train, truth = f.transformTrain('../data/train.csv')
-	print train.shape
+
 	c = classifier.Classifier(train, truth)
 	clf = LogisticRegression(C=3)
-	#clf = SGDClassifier(loss='log',penalty='l2',alpha=0.0001,n_iter=20,shuffle=True)
-	#c.validate(clf,nFolds=4,out='logr.csv')
-	#pl.show()
-	#return
+	clf = SGDClassifier(loss='log',penalty='l2',alpha=0.0001,n_iter=30,shuffle=True,random_state=1337)
+	c.validate(clf,nFolds=10,out='sgdBase30.csv')
+	print clf.coef_.shape
+	pl.figure()
+	pl.semilogy(np.sort(np.abs(clf.coef_[0.,:]))[::-1],np.arange(1,clf.coef_.shape[1]+1),lw=3)
+	pl.show()
+	x = np.abs(c.coef_)
+	sort_ = np.argsort(x)[::-1]
+
+#	clf = SGDClassifier(loss='log',penalty='l2',alpha=0.0001,n_iter=30,shuffle=True,random_state=1337)
+	clf = LogisticRegression(C=3)
+	c.validate(clf,indices=sort_[:int(0.5*sort_.size)],nFolds=10,out='sgd20Pct30.csv',coef=False)
+	print int(0.5*sort_.size)
+	return
 	test = f.transformTest('../data/test.csv')
 	print test.shape
-	clf.fit(train,truth)
-	y_ = clf.predict_proba(test)
+	clf.fit(train[:,sort_[:int(0.05*sort_.size)]],truth)
+	y_ = clf.predict_proba(test[:,sort_[:int(0.05*sort_.size)]])
 	print y_[:,1].min(), y_[:,1].max()
-	out = open('612.sub','w')
+	out = open('623ReducedFix.sub','w')
 	out.write('id,ACTION\n')
 	for i in xrange(y_.shape[0]):
 		out.write('%i,%f\n'%(i+1,y_[i,1]))
